96b4bd2aa9fd01204b16456ca9f8671bd2c602c3
BloveStar <blovestar59@gmail.com>
2018-05-15 10:30:32 +0800

    add mastar comm

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
             System.out.println("hahah");
         }
 
-        System.out.println("this is new branch!");
+        System.out.println("This is master!");
     }
 }
